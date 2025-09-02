15ccbaf5451d6ff40bdff879b02a60a30e91adad
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-09-02 12:12:39 +0100

    add in the OAI schema

--- src/main/java/org/javastro/ivoa/entities/IvoaJAXBContextFactory.java
+++ src/main/java/org/javastro/ivoa/entities/IvoaJAXBContextFactory.java
@@ -1,14 +1,14 @@
 /*
  * $Id: IvoaJAXBContextFactory.java,v 1.2 2011-09-13 13:43:33 pah Exp $
- * 
+ *
  * Created on 12 Mar 2008 by Paul Harrison (paul.harrison@manchester.ac.uk)
  * Copyright 2008 Astrogrid. All rights reserved.
  *
- * This software is published under the terms of the Astrogrid 
- * Software License, a copy of which has been included 
- * with this distribution in the LICENSE.txt file.  
+ * This software is published under the terms of the Astrogrid
+ * Software License, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
  *
- */ 
+ */
 
 package org.javastro.ivoa.entities;
 
@@ -22,27 +22,35 @@ import jakarta.xml.bind.JAXBException;
  */
 public class IvoaJAXBContextFactory {
 
-    
-    private static JAXBContext jc = null;
 
-    public static JAXBContext newInstance() throws JAXBException
-    {
-	if(jc == null) 
-	  jc = JAXBContext.newInstance(
+	private static volatile JAXBContext jc;
 
-	  		  "org.javastro.ivoa.entities.resource"
-            + ":org.javastro.ivoa.entities.resource.dataservice"
-            + ":org.javastro.ivoa.entities.resource.registry"
-            + ":org.javastro.ivoa.entities.resource.registry.iface"
-            + ":org.javastro.ivoa.entities.resource.applications"
-            + ":org.javastro.ivoa.entities.resource.cone"
-            + ":org.javastro.ivoa.entities.resource.sia"
-            + ":org.javastro.ivoa.entities.resource.standard"
-            + ":org.javastro.ivoa.entities.resource.registry.oaipmh"
-	  		);
-	return jc;
+	public static JAXBContext newInstance() throws JAXBException
+	{
+		if(jc == null)
+			synchronized (IvoaJAXBContextFactory.class) {
+				if (jc == null)
+					jc = JAXBContext.newInstance(
 
-    }
+							"org.javastro.ivoa.entities.resource"
+									+ ":org.javastro.ivoa.entities.resource.dataservice"
+									+ ":org.javastro.ivoa.entities.resource.registry"
+									+ ":org.javastro.ivoa.entities.resource.registry.iface"
+									+ ":org.javastro.ivoa.entities.resource.applications"
+									+ ":org.javastro.ivoa.entities.resource.cone"
+									+ ":org.javastro.ivoa.entities.resource.sia"
+									+ ":org.javastro.ivoa.entities.resource.standard"
+									+ ":org.javastro.ivoa.entities.resource.registry.oaipmh"
+									+ ":org.javastro.ivoa.entities.resource.registry.dublincore"
+									+ ":org.javastro.ivoa.entities.resource.registry.dublincore.simple"
+					            + ":org.javastro.ivoa.entities.vosi.availability"
+                           + ":org.javastro.ivoa.entities.vosi.capabilities"
+                           + ":org.javastro.ivoa.entities.vosi.tables"
+               );
+			}
+		return jc;
+
+	}
 }
 
 
